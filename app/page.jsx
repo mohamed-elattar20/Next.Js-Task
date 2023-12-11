3db16e8a609f1b46a0d219279269260a6b22b97d
Mohamed Elattar <mohamedelattarm819@gmail.com>
2023-12-11 18:54:38 +0200

    Add Functions To Every Section Data

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,34 @@ const BASE_URL = "https://api-stage.spotx.app/api/v1";
 const getUiBuilders = async () => {
   const res = await fetch(`${BASE_URL}/ui-builders`);
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
   return data;
 };
 
+const getRegions = async (regionContent) => {
+  const res = await fetch(`${BASE_URL}/${regionContent}`);
+  return res.json();
+};
+const getSubRegions = async (regionContent, regionFilters) => {
+  const res = await fetch(
+    `${BASE_URL}/${regionContent}/${regionFilters.region}`
+  );
+  console.log(`${BASE_URL}/regions/${regionFilters.region}`);
+  return res.json();
+};
+const getUnits = async (unitsContent, unitsFilters) => {
+  const res = await fetch(
+    `${BASE_URL}/user/${unitsContent}?${new URLSearchParams(unitsFilters)}`
+  );
+
+  return res.json();
+};
+
 export default async function Home() {
-  const u = await getUiBuilders();
-  return <main></main>;
+  const res = await getUiBuilders();
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  );
 }
